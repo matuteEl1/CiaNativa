4337d57c368e4ad05638a1a6c85951fe52559dd2
matute <matute_a_p@hotmail.com>
2020-10-15 18:30:44 -0300

    Add project files.

---
+++ ArtsentApp/Form1.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace ArtsentApp
+{
+    partial class frmStock
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmStock));
+            this.tblMain = new System.Windows.Forms.TableLayoutPanel();
+            this.cboProducto = new System.Windows.Forms.ComboBox();
+            this.tblMain.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tblMain
+            // 
+            this.tblMain.BackgroundImage = global::ArtsentApp.Properties.Resources.NativaDesc;
+            this.tblMain.ColumnCount = 1;
+            this.tblMain.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tblMain.Controls.Add(this.cboProducto, 0, 0);
+            this.tblMain.Location = new System.Drawing.Point(5, 8);
+            this.tblMain.Name = "tblMain";
+            this.tblMain.RowCount = 2;
+            this.tblMain.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.tblMain.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 83.33334F));
+            this.tblMain.Size = new System.Drawing.Size(979, 585);
+            this.tblMain.TabIndex = 0;
+            // 
+            // cboProducto
+            // 
+            this.cboProducto.FormattingEnabled = true;
+            this.cboProducto.Location = new System.Drawing.Point(3, 3);
+            this.cboProducto.Name = "cboProducto";
+            this.cboProducto.Size = new System.Drawing.Size(121, 28);
+            this.cboProducto.TabIndex = 0;
+            // 
+            // frmStock
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Inherit;
+            this.AutoSize = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.ClientSize = new System.Drawing.Size(988, 599);
+            this.Controls.Add(this.tblMain);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "frmStock";
+            this.Text = "Stock Consolidado";
+            this.tblMain.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tblMain;
+        private System.Windows.Forms.ComboBox cboProducto;
+    }
+}
+
---
+++ ArtsentApp/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ArtsentApp
+{
+    public partial class frmStock : Form
+    {
+        public frmStock()
+        {
+            InitializeComponent();
+        }
+
+        private void lblProducto_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
